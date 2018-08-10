4e0bcf7b6e6de1c75c1e4346a30cf72d9804c6e0
Friedrich Rober <friedrich.rober@rwth-aachen.de>
2018-08-10 12:17:07 +0200

    Add testfiles

---
+++ tests/magma_testfiles/magma_Dihedral.m
@@ -0,0 +1,58 @@
+/* Read the testfile from root */
+
+testname := "MagmaDihedral";
+
+RF := recformat< name : Strings(), group, max_index : Integers() >;
+ToTest := [
+rec<RF | name := "Dih($10000$)", group := DihedralGroup(GrpFP, 10000), max_index := 125>,
+];
+
+for i in [1..#ToTest] do
+
+  SetProfile(true);
+
+  F := ToTest[i]`group;
+  L := LowIndexNormalSubgroups(F,ToTest[i]`max_index);
+  index := [x`Index : x in L];
+  supers := [x`Supergroups : x in L];
+  supers := [IndexedSetToSequence(SetToIndexedSet(x)) : x in supers];
+
+  G := ProfileGraph();
+  V := VertexLabels(G);
+  Fcts := ["LowIndexNormalSubgroups", "FindTs", "FindPQuotients", "FindIntersections", "AddGroup"];
+  Times := [];
+  Counts := [];
+  for j in [1..#Fcts] do
+    f := [x : x in V | Fcts[j] in x`Name];
+    t := f[1]`Time;
+    t := Round(t * 10);
+    Times[j] := IntegerToString(t div 10) cat "." cat IntegerToString(t mod 10);
+    Counts[j] := f[1]`Count;
+  end for;
+  Fcts := ["\"" cat x cat "\"" : x in Fcts]; 
+  total_time := Times[1];
+
+  filename := "tests/magma_results/" cat testname cat IntegerToString(i);
+  SetOutputFile(filename: Overwrite := true);
+  PrintFile(filename, "MAGMA_index := ");
+  PrintFile(filename, index);
+  PrintFile(filename,  ";\n");
+  PrintFile(filename, "MAGMA_supers := ");
+  PrintFile(filename, supers);
+  PrintFile(filename, ";\n");
+  PrintFile(filename, "MAGMA_time := ");
+  PrintFile(filename, total_time);
+  PrintFile(filename, ";\n");
+  PrintFile(filename, "MAGMA_fcts := ");
+  PrintFile(filename, Fcts);
+  PrintFile(filename, ";\n");
+  PrintFile(filename, "MAGMA_times := ");
+  PrintFile(filename, Times);
+  PrintFile(filename, ";\n");
+  PrintFile(filename, "MAGMA_counts := ");
+  PrintFile(filename, Counts);
+  PrintFile(filename, ";\n");
+  UnsetOutputFile();
+  
+  ProfileReset();
+end for;
